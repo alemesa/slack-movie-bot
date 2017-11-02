a1e15f6403fa404dbe6dc2ba538ece73102933ee
Alejandro Mesa <alejandro.mesa1991@gmail.com>
2017-11-02 15:32:45 -0400

    more options

--- index.js
+++ index.js
@@ -26,21 +26,40 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   // if next or previous
   let text = req.body.text;
+  if (text == '') {
+    let image =
+      'https://images-na.ssl-images-amazon.com/images/M/MV5BOGJjNzZmMmUtMjljNC00ZjU5LWJiODQtZmEzZTU0MjBlNzgxL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY1200_CR90,0,630,1200_AL_.jpg';
 
-  let image =
-    'https://images-na.ssl-images-amazon.com/images/M/MV5BOGJjNzZmMmUtMjljNC00ZjU5LWJiODQtZmEzZTU0MjBlNzgxL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY1200_CR90,0,630,1200_AL_.jpg';
-
-  let data = {
-    response_type: 'in_channel', // public to the channle
-    text: 'Spirited Away (2001) - Hayao Miyasaki - Tuesday 14 November',
-    attachments: [
-      {
-        pretext: 'Poster by ....',
-        color: '#231F20',
-        image_url: image,
-        footer: 'Movie - Night'
-      }
-    ]
-  };
-  res.json(data);
+    let data = {
+      response_type: 'in_channel', // public to the channle
+      text: 'Spirited Away (2001) - Hayao Miyasaki - Tuesday 14 November',
+      attachments: [
+        {
+          pretext: 'Poster by @steven',
+          color: '#231F20',
+          image_url: image,
+          footer: 'Ocean (3rd Room Couch & TV) 6:30-9pm'
+        }
+      ]
+    };
+    res.json(data);
+  } else if (text == 'previous') {
+    let data = {
+      response_type: 'in_channel', // public to the channle
+      text:
+        'Alien (1979) - Ridley Scott - Tuesday 17 October\nBlade Runner (1982) - Ridley Scott - Tuesday 3 October',
+      attachments: [
+        {
+          color: '#231F20'
+        }
+      ]
+    };
+    res.json(data);
+  } else {
+    let data = {
+      response_type: 'ephemeral', // public to the channle
+      text: "You're doing it wrong either type /movie or /movie previous"
+    };
+    res.json(data);
+  }
 });
